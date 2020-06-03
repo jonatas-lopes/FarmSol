a019431efde359b14dc2d53d4d976e40dfa509d5
jonatas-lopes <jonatas.lopes95@live.com>
2020-06-03 13:46:45 -0300

    Mudança na conexao

--- conexao.php
+++ conexao.php
@@ -2,16 +2,17 @@
 
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
-$servidor = 'localhost';
+$servidor = '200.200.200.131:3306';
 $database = 'farmsol';
-$usuario = 'root';
+$usuario = 'gsis';
+$senha = 'Cheatreveal95&';
 
 
 
 
 global $con;
 
-$con = mysqli_connect($servidor, $usuario ) or die (mysqli_error());
+$con = mysqli_connect($servidor, $usuario, $senha ) or die (mysqli_error());
 
 mysqli_select_db($con, $database) or die (mysqli_error($con));
 
